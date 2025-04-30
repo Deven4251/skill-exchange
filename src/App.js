5235638a644cc20cea583c2d0e9bf4449e8f24dd
Devendra Mishra <devendramishra778585@gmail.com>
2025-04-30 09:57:02 +0530

    commit changes in public

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { FaGraduationCap } from 'react-icons/fa';
 
-function App() {
+const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="home-container">
+      <nav className="navbar">
+        <div className="logo">Logo</div>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Skills</li>
+          <li>Lets Discuss</li>
+          <li>Chat</li>
+          <li>Signup/Login</li>
+          <li>Profile</li>
+        </ul>
+      </nav>
+
+
+<div className="container-fluid P-5">
+        <div className="row P-5">
+          <div className="col-md-6 p-5">
+          <FaGraduationCap className="cap-icon" />
+          </div>
+          <div className="col-md-6 p-5">
+          <p className="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <span className="highlight"> Sed do eiusmod tempor incididunt ut labore et dolore </span>
+          magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+          <span className="highlight"> maecenas accumsan lacus </span>
+          vel facilisis. Lorem donec massa sapien faucibus et molestie ac feugiat. Sed lectus
+          vestibulum mattis ullamcorper velit sed.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          </div>
+        </div>
+        </div>
     </div>
   );
-}
+};
 
-export default App;
+export default HomePage;
